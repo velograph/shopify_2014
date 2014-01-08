f0f53b9e0c8e08ba377284f1cee00e60ee6228cf
sellwood <velograph@gmail.com>
2014-01-07 16:41:01 -0800

    Dialog created and styled for product that is or isn't for sale. Created a small icon for the closing of the dialog. The theme.liquid file has the external css for the dialog simply commented out. I'm going to remove the comment in the upcoming commit.

--- assets/site_scripts.js
+++ assets/site_scripts.js
@@ -4,3 +4,25 @@ $(function() {
     return false;
   });
 });
+
+$(function() {
+  $( "#dialog" ).dialog({
+    autoOpen: false,
+    modal: true,
+    title: "Purchasing Methods",
+    width: 500,
+    show: {
+      effect: "fade",
+      duration: 500
+    },
+    hide: {
+      effect: "fade",
+      duration: 500
+    }
+  });
+
+  $( "#opener" ).click(function() {
+    $( "#dialog" ).dialog( "open" );
+  });
+});
+
